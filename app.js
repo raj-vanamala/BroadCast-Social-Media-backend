8e503e6a3378f001a099cc8a8c019326e194d82d
hainag16 <36581076+hainag16@users.noreply.github.com>
2020-08-15 20:48:17 +0530

    partial code

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/', usersRouter);
